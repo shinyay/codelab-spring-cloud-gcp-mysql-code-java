7a53161048bc6cba335282885963f5bdc769a040
shinyay <shinya.com@gmail.com>
2021-09-14 13:34:30 +0900

    Add a repository for Employee

---
+++ src/main/java/com/google/codelab/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.google.codelab.repository;
+
+import com.google.codelab.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
