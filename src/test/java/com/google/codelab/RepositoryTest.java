373459b5ad5191936df8404396b3c96d69a0c426
shinyay <shinya.com@gmail.com>
2021-09-14 13:47:43 +0900

    Add Repository layer unit test with Testcontainer

--- src/test/java/com/google/codelab/RepositoryTest.java
+++ src/test/java/com/google/codelab/RepositoryTest.java
@@ -1,5 +1,10 @@
 package com.google.codelab;
 
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@DataJpaTest
 public class RepositoryTest {
 
 }
