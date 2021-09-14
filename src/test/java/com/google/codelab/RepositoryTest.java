3b467c9acfca8e29207718a684c232350cc39d90
shinyay <shinya.com@gmail.com>
2021-09-14 17:25:07 +0900

    Add MySQL Container configuration

--- src/test/java/com/google/codelab/RepositoryTest.java
+++ src/test/java/com/google/codelab/RepositoryTest.java
@@ -1,10 +1,32 @@
 package com.google.codelab;
 
+import com.google.codelab.entity.Employee;
+import com.google.codelab.repository.EmployeeRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.List;
 
 @Testcontainers
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class RepositoryTest {
+
+    @Autowired
+    private EmployeeRepository repository;
+
+    @Container
+    private static final MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:5.7.34")
+            .withDatabaseName("test-db")
+            .withUsername("scott")
+            .withPassword("tiger");
+
 
 }
