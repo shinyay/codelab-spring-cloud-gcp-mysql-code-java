f4a48e0c4afab1b49165f9d7f8f2a93679332ab7
shinyay <shinya.com@gmail.com>
2021-09-16 08:47:02 +0900

    Modify alter table to specify column position

--- src/test/resources/db/migration/V3__Alter_Employee_Add_DepartmentId.sql
+++ src/test/resources/db/migration/V3__Alter_Employee_Add_DepartmentId.sql
@@ -1,4 +1,5 @@
 ALTER TABLE employee
-    ADD department_id integer NOT NULL;
+    ADD department_id integer NOT NULL
+    AFTER employee_id;
 ALTER TABLE employee
     ADD FOREIGN KEY (department_id) REFERENCES department (department_id);
