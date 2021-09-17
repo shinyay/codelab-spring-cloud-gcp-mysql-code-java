82b4461ba1ed61220460bb3822a2c37ae3daee82
shinyay <shinya.com@gmail.com>
2021-09-17 14:23:51 +0900

    Add service layer for Employee Repository

---
+++ src/main/java/com/google/codelab/service/EmployeeService.java
@@ -0,0 +1,50 @@
+package com.google.codelab.service;
+
+import com.google.codelab.entity.Employee;
+import com.google.codelab.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeService {
+
+    private final EmployeeRepository repository;
+
+    public EmployeeService(EmployeeRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Employee> findAllEmployees() {
+        return repository.findAll();
+    }
+
+    public List<Employee> findEmployeeByDepartmentId(Long employeeId) {
+        return repository.findByDepId(employeeId);
+    }
+
+    public Employee registerEmployee(Employee employee) {
+        return repository.save(employee);
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        Optional<Employee> registeredEmployee = repository.findById(employee.getEmployee_id());
+        if(registeredEmployee.isPresent()) {
+            var registeredEmployeeEntity = registeredEmployee.get();
+            registeredEmployeeEntity.setEmployee_id(employee.getEmployee_id());
+            registeredEmployeeEntity.setName(employee.getName());
+            registeredEmployeeEntity.setRole(employee.getRole());
+            registeredEmployeeEntity.setDepartment_id(employee.getDepartment_id());
+            return repository.save(registeredEmployeeEntity);
+        } else {
+            return repository.save(employee);
+        }
+    }
+
+    public void deleteEmployee(Long employeeId) {
+        repository.deleteById(employeeId);
+    }
+
+}
