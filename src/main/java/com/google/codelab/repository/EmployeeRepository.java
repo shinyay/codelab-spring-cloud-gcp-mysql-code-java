a8be64d554466d3257973b742c290afc6fe70941
shinyay <shinya.com@gmail.com>
2021-09-17 16:49:14 +0900

    Add find employee by employee id

--- src/main/java/com/google/codelab/repository/EmployeeRepository.java
+++ src/main/java/com/google/codelab/repository/EmployeeRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    List<Employee> findByDepId(Long id);
+    Optional<Employee> findByEmpId(Long employee_id);
+    List<Employee> findByDepId(Long department_id);
 }
