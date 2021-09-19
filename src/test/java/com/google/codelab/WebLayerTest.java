41d70d92a0724a9b4067cd94a9fde071b862fb15
shinyay <shinya.com@gmail.com>
2021-09-20 06:47:21 +0900

    Add WebMvcTest class for Employee Controller

---
+++ src/test/java/com/google/codelab/WebLayerTest.java
@@ -0,0 +1,8 @@
+package com.google.codelab;
+
+import com.google.codelab.controller.EmployeeController;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(EmployeeController.class)
+public class WebLayerTest {
+}
