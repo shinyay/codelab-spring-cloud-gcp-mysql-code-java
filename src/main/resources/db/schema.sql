bf108f2befb0506da7986022fef4b605b9639bf3
shinyay <shinya.com@gmail.com>
2021-09-23 14:04:17 +0900

    Add schema  for Cloud SQL

---
+++ src/main/resources/db/schema.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS department;
+CREATE TABLE department
+(
+    department_id   integer     NOT NULL AUTO_INCREMENT,
+    department_name varchar(32) NOT NULL UNIQUE,
+    PRIMARY KEY (department_id)
+);
+
+DROP TABLE IF EXISTS employee;
+CREATE TABLE employee
+(
+    employee_id   integer     NOT NULL AUTO_INCREMENT,
+    employee_name varchar(64) NOT NULL UNIQUE,
+    role          varchar(32) DEFAULT NULL,
+    PRIMARY KEY (employee_id)
+);
+
+ALTER TABLE employee
+    ADD FOREIGN KEY (department_id) REFERENCES department (department_id);
