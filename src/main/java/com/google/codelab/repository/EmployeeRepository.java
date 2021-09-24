68af6ce981e6bedcab7346ba40d0afa0924dc7bc
shinyay <shinya.com@gmail.com>
2021-09-24 09:10:52 +0900

    Remove the repository interface for tutorial

--- src/main/java/com/google/codelab/repository/EmployeeRepository.java
+++ src/main/java/com/google/codelab/repository/EmployeeRepository.java
@@ -1,13 +1,4 @@
 package com.google.codelab.repository;
 
-import com.google.codelab.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-import java.util.Optional;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    List<Employee> findByDepId(Long department_id);
+public interface EmployeeRepository {
 }
