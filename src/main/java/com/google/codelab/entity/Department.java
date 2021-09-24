2c7cbf8b97524ff04ae807a526ab8d211cebf0cd
shinyay <shinya.com@gmail.com>
2021-09-24 09:11:37 +0900

    Remove the department entity for tutorial

--- src/main/java/com/google/codelab/entity/Department.java
+++ src/main/java/com/google/codelab/entity/Department.java
@@ -1,31 +1,4 @@
 package com.google.codelab.entity;
 
-import javax.persistence.*;
-
-@Entity
-@Table(name = "department")
 public class Department {
-    @Id
-    @GeneratedValue
-    @Column(name = "department_id", nullable = false)
-    private Long id;
-
-    @Column(name = "department_name", nullable = false, length = 32)
-    private String name;
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
